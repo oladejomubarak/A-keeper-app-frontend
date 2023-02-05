fb2f6ce8f6efc6379dede7b527200bc77f0b068a
oladejomubarak <oladejomubarakade@gmail.com>
2023-02-05 05:38:50 +0000

    designing footer

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Footer(){
+  return(
+    <div>
+      <footer>
+      Copyright C {new Date().getFullYear()}
+      </footer>
+    </div>
+  );
+}
+export default Footer;
---
+++ src/components/Note.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Note(){
+  return(
+    <div className="note">
+      <h1>This is a title</h1>
+      <p>This s a paragraph</p>
+    </div>
+  );
+}
+export default Note;
