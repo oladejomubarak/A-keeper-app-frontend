dddbb2a0ff50858e761198d9a34d05510ef43fff
oladejomubarak <oladejomubarakade@gmail.com>
2023-02-09 19:40:43 +0000

    mapping items from external function

--- src/components/App.js
+++ src/components/App.js
@@ -6,7 +6,7 @@ import "../style/styles.css"
 import notes from "../notes";
 
 function createNote(noteItem){
-return <Note title = {noteItem.title} content = {noteItem.content}/>
+return <Note key={noteItem.key} title = {noteItem.title} content = {noteItem.content}/>
 }
 function App() {
   return (
