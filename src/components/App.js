eb3b8778e2a5836591c7f0e74a51599529c866a1
oladejomubarak <oladejomubarakade@gmail.com>
2023-02-09 12:33:19 +0000

    creating content

--- src/components/App.js
+++ src/components/App.js
@@ -3,12 +3,17 @@ import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
 import "../style/styles.css"
+import notes from "../notes";
 
+function createNote(noteItem){
+return <Note title = {noteItem.title} content = {noteItem.content}/>
+}
 function App() {
   return (
     <div>
       <Header />
-      <Note />
+       {notes.map(createNote)}
+      {/* <Note title="This is the note title" content="This is the note content" /> */}
       <Footer />
     </div>
   );
