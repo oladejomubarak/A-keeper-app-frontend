63b9610dc9c1e9ed25a64d0a1eb5b57c36d48b40
oladejomubarak <oladejomubarakade@gmail.com>
2023-02-18 21:07:25 +0000

    implementing add note functionality

--- src/components/CreateArea.js
+++ src/components/CreateArea.js
@@ -4,12 +4,25 @@ function CreateArea() {
   const [note, setNote] = useState({
     title:"",
     content:""
-  }); 
+  });
+  function handleChange(event){
+    const [name , value] = event.target;
+
+    setNote(previousValue=> {
+      return{
+        ...previousValue,
+        [name]: value
+      }
+    });
+
+  }
   return (
     <div>
       <form>
-        <input name="title" placeholder="Title" />
-        <textarea name="content" placeholder="Take a note..." rows="3" />
+        <input name="title" value={note.title} placeholder="Title"
+        onChange={handleChange} />
+        <textarea name="content" value={note.content} placeholder="Take a note..." rows="3"
+        onChange={handleChange} />
         <button>Add</button>
       </form>
     </div>
